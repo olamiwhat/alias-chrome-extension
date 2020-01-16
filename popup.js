a6ac009caedafe0ce0cc2143235902460899b80f
Ola Adedoyin <olalinux03@gmail.com>
2020-01-16 03:08:39 -0500

    rearrange content display

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-restricted-syntax */
 const getProfile = document.createElement('button');
 getProfile.setAttribute('id', 'getProfile_btn');
 getProfile.innerText = 'Get Profile';
@@ -13,7 +15,6 @@ getProfileAction.onclick = function getProfileDetails() {
       const person = data;
       console.log(person);
       const infoDiv = document.getElementById('popup');
-      console.log('infoDiv: ', infoDiv);
       if (infoDiv.innerHTML) {
         infoDiv.innerHTML = '';
       }
@@ -27,17 +28,18 @@ getProfileAction.onclick = function getProfileDetails() {
           if (key === 'birthday') {
             const t = document.createTextNode(`${key.toUpperCase()}: ${Object.values(person[key])[0]}`);
             p.append(t);
-          }
-          // else if (key === 'credit_card') {
-          //   const ccV = Object.values(person[key]);
-          //   const ti = document.createTextNode(`${key.toUpperCase()}: expiration: ${ccV[0]} number: ${ccV[1]}, pin: ${ccV[2]}, security: ${ccV[3]}`);
-          //   p.append(ti);
-          // }
-          else if (key === 'photo') {
+          } else if (key === 'credit_card') {
+            continue;
+          } else if (key === 'photo') {
+            const img = document.getElementById('profile_picture');
+            if (img) {
+              img.remove();
+            }
             const pp = document.createElement('img');
             pp.id = 'profile_picture';
             pp.setAttribute('src', `${person[key]}`);
-            p.append(pp);
+            document.getElementById('heading').appendChild(pp);
+            // p.append(pp);
           } else if (key === 'name') {
             const t = document.createTextNode(`${key.toUpperCase()}: ${person[key]} ${data.surname}`);
             p.append(t);
